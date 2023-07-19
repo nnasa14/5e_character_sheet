455c419d3a066d54e701ce0ea486d5992e89eab4
nnasa14 <nnascimento.kr@gmail.com>
2023-07-19 18:27:13 -0400

    Added main.py and char info class

---
+++ __main__.py
@@ -0,0 +1,7 @@
+#biographical info (char name, class & level, background, player name, race, alignment, xp)
+#stats (str, dex, con, int, wis, cha)
+#save throws (str, dex, con, int, wis, cha)
+#skills [organize byt highest num](acrobatics, animal handling, arcana, athletics, deception, history, 
+#   insight, intimidation, investigation, investigation, medicine, nature, perception
+#   performance, persuasion, religion, sleight of hand, stealth, survival)
+#combat stats (AC, initiative, speed)
